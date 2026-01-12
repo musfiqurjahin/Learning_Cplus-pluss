78e18aec2fe9fa792a21b2b059d05b689db45a26
Musfiqur Jahin <119739345+musfiqurjahin@users.noreply.github.com>
2026-01-12 20:21:32 +0600

    Deploy

---
+++ First_program/Line_break.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    std::cout <<"Hello,m Jahin" <<std::endl;  //type-1    <<std::endl
+    std::cout <<"I love coding"<< '\n';
+    std::cout << '\n';  //type-2  <<'\n' 
+    std::cout <<"J" <<'\n';
+    std::cout <<"A" <<'\n';
+    std::cout <<"H" <<'\n';
+    std::cout <<"I" <<'\n';
+    std::cout <<"N" <<'\n';
+    return 0;
+}
