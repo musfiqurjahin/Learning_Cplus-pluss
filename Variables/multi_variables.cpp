996939480dbaa8b24b0e276c175be3f17033559a
Musfiqur Jahin <119739345+musfiqurjahin@users.noreply.github.com>
2026-01-12 20:42:39 +0600

    Deploy-2

---
+++ Variables/multi_variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    int x =  5;
+    int y =  7;
+    int summation = x + y;
+
+    std::cout  <<"x : " << x << '\n';
+    std::cout <<"y : " << y << '\n';
+
+    std::cout << "Summation of x and y: " << summation << '\n';
+
+
+
+    
+
+
+
+
+    return 0;
+}
---
+++ Variables/single_value.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    
+    int x; //declaration of unique identifier 
+    x = 57; //assignment
+
+    std::cout << x ;
+
+    return 0;
+}
