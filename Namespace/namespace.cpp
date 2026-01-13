7c37b646905fdf5d5aa18378aa093042a9f91c24
Musfiqur Jahin <119739345+musfiqurjahin@users.noreply.github.com>
2026-01-13 22:22:17 +0600

    namespace

---
+++ Namespace/namespace.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+//Namespace provides a solution for preventing name conflicts 
+//in learge project. Each entity needs a unique name.
+
+//A namespace allows for indentically named entities as long as the newspapaer
+
+
+
+
+
+namespace first {
+    int x = 1;
+}
+
+namespace second {
+    int x = 2;
+}
+
+int main()  {
+
+    int x = 0 ;
+
+
+    std::cout << first::x  << '\n';
+
+    std::cout << second::x << '\n';
+
+
+    return 0 ;
+}
---
+++ Namespace/usingprefix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+//Namespace provides a solution for preventing name conflicts 
+//in learge project. Each entity needs a unique name.
+
+//A namespace allows for indentically named entities as long as the newspapaer
+
+
+
+
+
+namespace first {
+    int x = 1;
+}
+
+namespace second {
+    int x = 2;
+}
+
+int main()  {
+
+    int x = 0 ;
+
+
+    //using prefix:
+    using namespace second;
+
+    std::cout << x  << '\n';
+
+
+
+    return 0 ;
+}
